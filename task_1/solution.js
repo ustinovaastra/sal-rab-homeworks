903f372b25d7d2d70186cf9f6847a44099a8b14b
Olga Ustinova <olga_ustinova@icloud.com>
2021-10-28 22:09:28 +0300

    task 1.1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -2,6 +2,17 @@ function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
 
+    let message;
+    message = productName;
+    message += ' ';
+    message += 'за';
+    message += ' ';
+    message += productPrice;
+    message += ' ';
+    message += 'теперь в корзине!';
+
+    console.log(message);
+
     // Задание №1.1. Оповещение о добавлении в корзину
 
     // создайте переменную message
