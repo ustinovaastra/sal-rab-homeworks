81cb92f9b900247412b405c617948f03120866e4
Olga Ustinova <olga_ustinova@icloud.com>
2021-11-11 21:48:53 +0300

    final task done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,23 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
-
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    const data = {
+        client: `${name} ${phone}`,
+        order: {
+            address: `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`,
+            sum
+        },
+        goods: []
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    for (let i = 0; i < goods.length; i++) {
+        const {title, count} = goods[i];
 
-    data.client = 'Иван';
+        data.goods.push({
+            title,
+            count
+        });
+    }
 
-    let jsonData = JSON.stringify(data);
-
-    return jsonData;
+    return JSON.stringify({data});
 }
